459db32dbc5235deb3e5f1f58952a7cdf84f1ae4
riveranb <riveranb@gmail.com>
2019-12-02 12:06:21 +0800

    try main camera control

---
+++ Assets/MainCamControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamControl : MonoBehaviour
+{
+    public Camera[] mOtherCams;
+    private bool mFixedDone = false;
+
+    private void FixedUpdate()
+    {
+        if (mFixedDone)
+        {
+            return;
+        }
+
+        foreach (var c in mOtherCams)
+        {
+            c.gameObject.SetActive(false);
+        }
+        mFixedDone = true;
+    }
+
+    private void LateUpdate()
+    {
+        mFixedDone = false;
+        foreach (var c in mOtherCams)
+        {
+            c.gameObject.SetActive(true);
+        }
+    }
+}
